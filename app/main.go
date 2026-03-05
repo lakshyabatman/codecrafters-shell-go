19005189c0cbe2b8468dbbc23f7bf1dbd67f69ec
Lakshya Khera <lakshya.khera@gmail.com>
2026-03-06 02:33:57 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -5,23 +5,40 @@ import (
 	"strings"
 	"bufio"
 	"os"
+	"slices"
 	)
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Print
 
+var supportedCommands = []string{"echo", "exit", "type"}
+
+
 func main() {
 	for {
 		command := controlCommand()
+		
        		if command[0] == "exit" {
 			break		
-		} else if command[0] == "echo" {
-			fmt.Println(strings.Join(command[1:], " "))
-		}else {	
-			fmt.Println(command[0] + ": command not found")
+		}else {
+			handleCommand(command)
 		}	
 	}
 }
+func handleCommand(command []string) {
+	if command[0] == "echo" {
+		fmt.Println(strings.Join(command[1:], " "))
+	} else if command[0] == "type" && slices.Contains(supportedCommands, command[1]) {
+	
+		fmt.Println(command[1] + " is a shell builtin")			
+	
+	}else  {    
+		fmt.Println(command[0] + ": command not found")
+}
+
+}
+
+
 
 func controlCommand() []string {
 	fmt.Print("$ ")
