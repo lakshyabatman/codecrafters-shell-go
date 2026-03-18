4931d66dff710727e4d8a53c43611e26d0a5b50a
Lakshya Khera <lakshya.khera@gmail.com>
2026-03-19 01:23:07 +0530

    codecrafters submit [skip ci]

--- app/bellcompleter/bellcompleter.go
+++ app/bellcompleter/bellcompleter.go
@@ -2,6 +2,8 @@ package bellcompleter
 
 import (
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/chzyer/readline"
 )
@@ -12,8 +14,33 @@ type BellCompleter struct {
 
 func (b *BellCompleter) Do(line []rune, pos int) ([][]rune, int) {
 	runes, level := b.Completer.Do(line, pos)
+	fmt.Println(level)
+	if len(runes) == 0 {
+		paths := strings.Split(os.Getenv("PATH"), ":")
+		candidates := [][]rune{}
+		for _, path := range paths {
+			if files, err := os.ReadDir(path); err == nil {
+				for _, file := range files {
+					if file.Type().IsRegular() && strings.HasPrefix(file.Name(), string(line)) {
+						if len(file.Name()) > 0 {
+							candidates = append(candidates, []rune(file.Name()))
+
+						}
+
+					}
+				}
+			}
+			if len(candidates) > 0 {
+				// fmt.Println(candidates)
+				return candidates, len(line)
+			}
+		}
+
+	}
+
 	if len(runes) == 0 {
 		fmt.Print("\x07")
 	}
+
 	return runes, level
 }
