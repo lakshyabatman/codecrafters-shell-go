dda18eb119c3b720dee1cb5964f723d0bc438f27
Lakshya Khera <lakshya.khera@gmail.com>
2026-04-05 15:20:31 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -183,14 +183,20 @@ func appendHistoryIntoFile(output string) {
 }
 
 func createNewHistory(output string) {
-	historyData, _ := os.ReadFile(history)
+	historyData, e := os.ReadFile(history)
+	if e != nil {
+		panic(e)
+	}
 	lines := strings.Split(strings.TrimRight(string(historyData), "\n"), "\n")
 
 	existingFileLines, _ := os.ReadFile(output)
 	newLines := strings.Split(strings.Trim(string(existingFileLines), "\n"), "\n")
 	newLines = append(newLines, lines[historyAppendOffset:]...)
 	historyAppendOffset = len(lines)
-	file, _ := os.OpenFile(output, os.O_CREATE|os.O_WRONLY, 0644)
+	file, e := os.OpenFile(output, os.O_CREATE|os.O_WRONLY, 0644)
+	if e != nil {
+		panic(e)
+	}
 	defer file.Close()
 	for _, line := range newLines {
 		file.WriteString(line + "\n")
